5145c705e3e3e2de29df0d8b1c5785edeae13529
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-20 14:42:01 +0530

    Refactor Light class: Update constructor to use default name "Light" and add getter and setter for brightness

--- src/smarthomesystem/Light.java
+++ src/smarthomesystem/Light.java
@@ -4,9 +4,15 @@ public class Light extends Device {
     
     int brightness;
 
-    public Light(String name) {
-        super(name);
+    public Light() {
+        super("Light");
     }
     
+    public void setBrightness(int brightness){
+        this.brightness = brightness;
+    }
     
+    public int getBrightness(){
+        return brightness;
+    }
 }
