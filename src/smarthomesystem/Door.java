c8290f6355b5c25c01d74147b749bca442fa363c
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-20 14:43:40 +0530

    Refactor Door class: Rename from Light, add lock/unlock functionality and lock status retrieval

--- src/smarthomesystem/Door.java
+++ src/smarthomesystem/Door.java
@@ -1,12 +1,23 @@
 
 package smarthomesystem;
-public class Light extends Device {
+public class Door extends Device {
     
-    int brightness;
+   
+   private boolean locked; 
 
-    public Light(String name) {
-        super(name);
+    public Door() {
+        super("Door");
     }
     
+    public void lock(){
+        locked = true;
+    }
     
+    public void unlock(){
+        locked = false;
+    }
+    
+    public String getLockStatus(){
+        return locked ? "LOCKED" : "UNLOCKED";
+    }
 }
